048359f719b67561b29f1fab3e84da240e35acb6
Darren Clarke <darren@redaranj.com>
2022-07-24 21:01:38 +0200

    Testimonials updates

--- components/common/TestimonialsSection.tsx
+++ components/common/TestimonialsSection.tsx
@@ -1,23 +1,21 @@
 import { FC } from "react";
-import { Box } from "@mui/material";
-import { useTranslate } from "react-polyglot";
-import { typography, colors } from "styles/theme";
+import { Grid } from "@mui/material";
+import { colors } from "styles/theme";
 import { PageSection } from "components/common/PageSection";
 import { HorizontalCarousel } from "components/common/HorizontalCarousel";
 
 export const TestimonialsSection: FC = ({ children }) => {
-  const t = useTranslate();
-  const { h2 } = typography;
   const { lightGrey } = colors;
+  const [first, ...rest] = children as any[];
 
   return (
     <PageSection backgroundColor={lightGrey}>
-      <Box
-        component="h2"
-        sx={{ ...h2, mb: "60px" }}
-        dangerouslySetInnerHTML={{ __html: t("testimonialsTitle") }}
-      />
-      <HorizontalCarousel>{children}</HorizontalCarousel>
+      <Grid container direction="column">
+        <Grid item>{first}</Grid>
+        <Grid item>
+          <HorizontalCarousel>{rest}</HorizontalCarousel>
+        </Grid>
+      </Grid>
     </PageSection>
   );
 };
