0da84019a8dd243abe8c2d33f1a1731a962b65ce
Darren Clarke <darren@redaranj.com>
2022-07-28 12:09:26 +0000

    Add ToggleButton

---
+++ components/common/ToggleButton.tsx
@@ -0,0 +1,60 @@
+import { FC, PropsWithChildren } from "react";
+import { Button } from "@mui/material";
+import { colors, breakpoints } from "styles/theme";
+
+type ToggleButtonProps = PropsWithChildren<{
+  on: boolean;
+  onClick?: () => void;
+}>;
+
+export const ToggleButton: FC<ToggleButtonProps> = ({
+  on = false,
+  children,
+  onClick,
+}) => {
+  const { black, white, lightGrey, mediumGrey, turquoise, lightTurquoise } =
+    colors;
+
+  return (
+    <Button
+      sx={{
+        lineHeight: "24px",
+        [breakpoints.ps]: {
+          fontSize: "14px",
+        },
+        [breakpoints.pl]: {
+          fontSize: "14px",
+        },
+        [breakpoints.ts]: {
+          fontSize: "14px",
+        },
+        [breakpoints.tl]: {
+          fontSize: "14px",
+        },
+        [breakpoints.ds]: {
+          fontSize: "16px",
+        },
+        [breakpoints.dl]: {
+          fontSize: "18px",
+        },
+        borderRadius: "500px",
+        fontWeight: 700,
+        textTransform: "none",
+        color: black,
+        backgroundColor: on ? lightTurquoise : lightGrey,
+        border: `2px solid ${on ? lightTurquoise : mediumGrey}`,
+        p: 1,
+        m: 0,
+        pl: "30px",
+        pr: "30px",
+        "&:hover": {
+          backgroundColor: turquoise,
+          color: white,
+        },
+      }}
+      onClick={onClick}
+    >
+      {children}
+    </Button>
+  );
+};
--- components/faq/QuestionsSection.tsx
+++ components/faq/QuestionsSection.tsx
@@ -1,11 +1,21 @@
-import { FC } from "react";
-import { Grid } from "@mui/material";
+import { FC, useState, useCallback } from "react";
+import { Box, Grid } from "@mui/material";
 import { colors, typography } from "styles/theme";
 import { PageSection } from "components/common/PageSection";
+import { ToggleButton } from "components/common/ToggleButton";
 
 export const QuestionsSection: FC = ({ children }) => {
   const { lightGrey, mediumGrey } = colors;
   const { bodyLarge } = typography;
+  const categories = ["About OpenArchive", "Save", "Support / Donations"];
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const toggleCategory = useCallback(
+    (category) =>
+      selectedCategory === category
+        ? setSelectedCategory(null)
+        : setSelectedCategory(category),
+    [selectedCategory, setSelectedCategory]
+  );
 
   return (
     <PageSection backgroundColor={lightGrey}>
@@ -27,7 +37,26 @@ export const QuestionsSection: FC = ({ children }) => {
           },
         }}
       >
-        {children}
+        <Grid
+          container
+          direction="row"
+          wrap="nowrap"
+          justifyContent="center"
+          spacing={3}
+          sx={{ mb: 6 }}
+        >
+          {categories.map((category) => (
+            <Grid item>
+              <ToggleButton
+                on={category === selectedCategory}
+                onClick={() => toggleCategory(category)}
+              >
+                {category}
+              </ToggleButton>
+            </Grid>
+          ))}
+        </Grid>
+        <Box>{children}</Box>
       </Grid>
     </PageSection>
   );
