3c6e36caaa46e540c57d59d68d82bb2d6f11b26f
Darren Clarke <darren@redaranj.com>
2022-07-31 17:24:54 +0000

    Fixes 18

--- components/partner/OpportunityItem.tsx
+++ components/partner/OpportunityItem.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
 import { Grid, Box } from "@mui/material";
+import { colors } from "styles/theme";
 import { loadImage } from "lib/frontendHelpers";
 
 export const OpportunityItem: FC = ({ children }) => {
   const [first, ...rest] = children as any[];
+  const { turquoise } = colors;
   const image = loadImage(first);
 
   return (
@@ -21,7 +23,23 @@ export const OpportunityItem: FC = ({ children }) => {
         />
       </Grid>
       <Grid item xs={6}>
-        <Box sx={{ px: 5, py: 10 }}>{rest}</Box>
+        <Box
+          sx={{
+            px: 5,
+            py: 10,
+            ul: { p: 0, listStyle: "none" },
+            "ul li::before": {
+              content: "'•'",
+              color: turquoise,
+              fontWeight: "bold",
+              display: "inline-block",
+              width: "10px",
+              m: 0,
+            },
+          }}
+        >
+          {rest}
+        </Box>
       </Grid>
     </Grid>
   );
