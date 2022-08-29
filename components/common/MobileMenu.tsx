e2ee3debe77dd654ceadbdef43e259302087b7c6
Darren Clarke <darren@redaranj.com>
2022-08-29 15:25:51 +0000

    Mobile menu fixes 1

--- components/common/MobileMenu.tsx
+++ components/common/MobileMenu.tsx
@@ -1,11 +1,9 @@
 import { FC } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { useTranslate } from "react-polyglot";
-import { Box, Button, Grid } from "@mui/material";
-import { colors, loader, typography } from "styles/theme";
+import { Box, Grid } from "@mui/material";
+import { colors, typography } from "styles/theme";
 import { FilledButton } from "components/common/FilledButton";
-import rightArrow from "public/images/right-arrow.svg";
 
 type MenuItemProps = {
   title: string;
@@ -15,19 +13,15 @@ type MenuItemProps = {
 const MenuItem: FC<MenuItemProps> = ({ title, url }) => {
   const { body } = typography;
   const { white } = colors;
+  const itemStyles = { ...body, fontWeight: 600, color: white, mb: 0 };
 
   return (
     <Grid item>
       <Link href={url} passHref>
-        <Box component="a">
+        <Box component="a" sx={{ textDecoration: "none" }}>
           <Grid container direction="row" spacing={1}>
             <Grid item>
-              <Box>
-                <Image src={rightArrow} alt="" loader={loader} />
-              </Box>
-            </Grid>
-            <Grid item>
-              <Box sx={{ ...body, fontWeight: 600, color: white }}>{title}</Box>
+              <Box sx={itemStyles}>{title}</Box>
             </Grid>
           </Grid>
         </Box>
@@ -43,6 +37,8 @@ type MobileMenuProps = {
 export const MobileMenu: FC<MobileMenuProps> = ({ closeMenu }) => {
   const t = useTranslate();
   const { white, black, turquoise } = colors;
+  const { body } = typography;
+  const headerStyles = { ...body, color: white, mb: 1 };
 
   return (
     <Box
@@ -51,35 +47,45 @@ export const MobileMenu: FC<MobileMenuProps> = ({ closeMenu }) => {
         color: white,
         height: "100vh",
         width: "100vw",
+        overflow: "scroll",
+        p: 3,
       }}
     >
-      <Box sx={{ backgroundColor: white, height: 50 }}>
-        <Button onClick={closeMenu}>Close</Button>
-      </Box>
-      <Grid
-        container
-        direction="column"
-        justifyContent="space-between"
-        sx={{ height: "75%", p: 4 }}
-      >
-        <Grid item container direction="column" spacing={6}>
+      <Grid container direction="column" justifyContent="space-between">
+        <Grid item container direction="column">
           <Grid item container direction="column">
-            <Grid item>{t("ourWork")}</Grid>
-            <MenuItem title={t("save")} url="/save" />
+            <Grid item>
+              <Box sx={headerStyles}>{t("ourWork")}</Box>
+            </Grid>
+            <MenuItem title={t("saveApp")} url="/save" />
             <MenuItem title={t("guides")} url="/guides" />
+            <MenuItem title={t("useCases")} url="/use" />
           </Grid>
           <Grid item container direction="column">
-            <Grid item>{t("about")}</Grid>
-            <MenuItem title={t("aboutUs")} url="/about" />
-            <MenuItem title={t("team")} url="/team" />
+            <Box sx={{ borderTop: `1px solid ${white}`, mt: 3, pt: 3 }}>
+              <MenuItem title={t("news")} url="/news" />
+            </Box>
           </Grid>
-        </Grid>
-        <Grid item sx={{ textAlign: "center" }}>
-          <Box sx={{ width: "100%" }}>
-            <FilledButton backgroundColor={white} textColor={black}>
-              {t("donate")}
-            </FilledButton>
-          </Box>
+          <Grid item container direction="column">
+            <Box sx={{ borderTop: `1px solid ${white}`, mt: 3, pt: 3 }}>
+              <Grid item>
+                <Box sx={headerStyles}>{t("about")}</Box>
+              </Grid>
+              <MenuItem title={t("aboutUs")} url="/about" />
+              <MenuItem title={t("team")} url="/team" />
+              <MenuItem title={t("partnerWithUs")} url="/partner" />
+              <MenuItem title={t("workWithUs")} url="/jobs" />
+              <MenuItem title={t("contactUs")} url="/contact" />
+              <MenuItem title={t("faq")} url="/faq" />
+            </Box>
+          </Grid>
+          <Grid item sx={{ textAlign: "center" }}>
+            <Box sx={{ width: "100%", mt: 6 }}>
+              <FilledButton backgroundColor={white} textColor={black}>
+                {t("donate")}
+              </FilledButton>
+            </Box>
+          </Grid>
         </Grid>
       </Grid>
     </Box>
