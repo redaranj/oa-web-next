1120868c9b6e550e2d856677533ce729279110a3
Darren Clarke <darren@redaranj.com>
2022-09-06 18:07:58 +0000

    Donate and conduct updates

--- components/conduct/ContentSection.tsx
+++ components/conduct/ContentSection.tsx
@@ -1,14 +1,17 @@
 import { FC } from "react";
 import { Box } from "@mui/material";
-import { colors } from "styles/theme";
+import { colors, typography } from "styles/theme";
 import { PageSection } from "components/common/PageSection";
 
 export const ContentSection: FC = ({ children }) => {
   const { white } = colors;
+  const { body } = typography;
 
   return (
     <PageSection backgroundColor={white}>
-      <Box sx={{ maxWidth: "710px", margin: "0 auto" }}>{children}</Box>
+      <Box sx={{ maxWidth: "710px", margin: "0 auto", "> li": body }}>
+        {children}
+      </Box>
     </PageSection>
   );
 };
