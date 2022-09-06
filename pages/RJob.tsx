6a96645cb803315391d629ef08f161057514f45b
Darren Clarke <darren@redaranj.com>
2022-09-06 22:30:22 +0000

    Remove redirect

--- pages/RJob.tsx
+++
@@ -1,9 +0,0 @@
-const RJob = () => <div />;
-
-export default RJob;
-
-export const getStaticProps = async () => ({
-  redirect: {
-    destination: "/jobs/decentralized-storage-researcher",
-  },
-});
