47738443a3020a0cfb88b6edb77272b6f8768c2d
Darren Clarke <darren@redaranj.com>
2022-09-06 21:21:58 +0000

    Add first redirect

--- next.config.js
+++ next.config.js
@@ -19,6 +19,15 @@ const config = withMDX({
     loader: "custom",
   },
   pageExtensions: ["ts", "tsx", "mdx"],
+  async redirects() {
+    return [
+      {
+        source: "/RJob",
+        destination: "/jobs/decentralized-storage-researcher",
+        permanent: true,
+      },
+    ];
+  },
 });
 
 export default config;
