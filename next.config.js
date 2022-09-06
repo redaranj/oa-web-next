27a43f171a6b6c416356bd95cdca2b5fbc20435b
Darren Clarke <darren@redaranj.com>
2022-09-06 20:22:41 +0000

    Remove asset prefix

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkMDXFrontmatter from "remark-mdx-frontmatter";
 import rehypeImageSize from "rehype-img-size";
 
-const basePath = !!process.env.CI ? "/web-next" : "";
-
 const withMDX = mdx({
   extension: /\.mdx?$/,
   options: {
@@ -17,8 +15,6 @@ const withMDX = mdx({
 
 const config = withMDX({
   reactStrictMode: true,
-  basePath,
-  assetPrefix: basePath,
   images: {
     loader: "custom",
   },
