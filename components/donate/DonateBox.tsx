25ccdd44379b3337d33dbad95aa820c2fc43ecd9
Darren Clarke <darren@redaranj.com>
2022-09-11 11:39:11 +0000

    Donate page updates

--- components/donate/DonateBox.tsx
+++ components/donate/DonateBox.tsx
@@ -6,69 +6,74 @@ export const DonateBox: FC<PropsWithChildren> = ({ children }) => {
   const { mediumBurgundy, darkBurgundy, turquoise, black, lightGrey, white } =
     colors;
   const { ps, ts } = breakpoints;
+  const [title, button, ...rest] = children as any[];
 
   return (
-    <Box
-      sx={{
-        maxWidth: 500,
-        m: "0 auto",
-        backgroundColor: lightGrey,
-        p: 3,
-        ul: {
-          display: "flex",
-          flexWrap: "wrap",
-          flexDirection: "row",
-          justifyContent: "flex-start",
-          p: 0,
-        },
-        li: {
+    <>
+      <Box
+        sx={{
+          maxWidth: 500,
+          m: "0 auto",
           backgroundColor: lightGrey,
-          border: `1px solid ${black}`,
-          borderRadius: 10,
-          display: "flex",
-          px: 3,
-          py: 1,
-          m: 1,
-          textAlign: "center",
-          fontWeight: "bold",
-          textColor: "bold",
-          width: "29%",
-          [ps]: { width: "40%" },
-          [ts]: { width: "29%" },
-          cursor: "pointer",
-          "&:hover": {
-            backgroundColor: turquoise,
-            border: `1px solid ${turquoise}`,
-            color: white,
+          p: 3,
+          ul: {
+            display: "flex",
+            flexWrap: "wrap",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            p: 0,
           },
-        },
-        "li em": {
-          fontStyle: "normal",
-          textAlign: "center",
-          display: "block",
-          width: "100%",
-        },
-        a: {
-          backgroundColor: mediumBurgundy,
-          textDecoration: "none",
-          textAlign: "center",
-          fontWeight: "bold",
-          color: white,
-          borderRadius: 10,
-          px: 3,
-          py: 1,
-          m: 0,
-          mb: 2,
-          display: "block",
-          width: "100%",
-          "&:hover": {
-            backgroundColor: darkBurgundy,
-            color: white,
+          li: {
+            backgroundColor: lightGrey,
+            border: `1px solid ${black}`,
+            borderRadius: 10,
+            display: "flex",
+            px: 3,
+            py: 1,
+            m: 1,
+            textAlign: "center",
+            fontWeight: "bold",
+            textColor: "bold",
+            width: "29%",
+            [ps]: { width: "40%" },
+            [ts]: { width: "29%" },
+            cursor: "pointer",
+            "&:hover": {
+              backgroundColor: turquoise,
+              border: `1px solid ${turquoise}`,
+              color: white,
+            },
           },
-        },
-      }}
-    >
-      {children}
-    </Box>
+          "li em": {
+            fontStyle: "normal",
+            textAlign: "center",
+            display: "block",
+            width: "100%",
+          },
+          a: {
+            backgroundColor: mediumBurgundy,
+            textDecoration: "none",
+            textAlign: "center",
+            fontWeight: "bold",
+            color: white,
+            borderRadius: 10,
+            px: 3,
+            py: 1,
+            m: 0,
+            mb: 2,
+            display: "block",
+            width: "100%",
+            "&:hover": {
+              backgroundColor: darkBurgundy,
+              color: white,
+            },
+          },
+        }}
+      >
+        {title}
+        {button}
+      </Box>
+      <Box sx={{ maxWidth: 500, pt: 6, m: "0 auto" }}>{rest}</Box>
+    </>
   );
 };
