880c765aecc1cb3a3932e766d87f4af58e6ed3d9
Darren Clarke <darren@redaranj.com>
2022-09-15 22:48:34 +0200

    Shrink donate image

--- components/donate/DonateSection.tsx
+++ components/donate/DonateSection.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Box, Grid } from "@mui/material";
 import { colors, breakpoints, loader } from "styles/theme";
 import { PageSection } from "components/common/PageSection";
-import donate2 from "public/images/donate-3.jpg";
+import donate2 from "public/images/donate-2.jpg";
 
 export const DonateSection: FC<PropsWithChildren> = ({ children }) => {
   const { white, darkBurgundy, mediumBurgundy } = colors;
