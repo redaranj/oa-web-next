4aad2364850359c3b32a9db91410adfb755a34a1
Vani Chitkara <64951124+vanichitkara@users.noreply.github.com>
2022-12-05 23:31:48 +0530

    Update VisionSection.tsx

--- components/about/VisionSection.tsx
+++ components/about/VisionSection.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Box, Grid } from "@mui/material";
 import { colors, breakpoints, loader, typography } from "styles/theme";
 import { PageSection } from "components/common/PageSection";
-import vision from "public/images/vision.png";
+import vision from "public/images/AboutUs/about-us-1.png";
 
 export const VisionSection: FC<PropsWithChildren> = ({ children }) => {
   const { white, lightGrey } = colors;
